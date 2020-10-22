6b6383bfe516dbd6bf245c320e1b9d112ceae28d
DESKTOP-G2RUKHA\F309 <D1074182102@gm.lhu.edu.tw>
2020-10-22 15:46:11 +0800

    2020/10/22

--- database/migrations/2020_10_22_055104_create_games_table.php
+++ database/migrations/2020_10_22_055104_create_games_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateGamesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->string('g_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('g_company');
+            $table->string('g_producer',50);
+            $table->foreign('g_company')->references('id')->on('games')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('games');
     }
 }
